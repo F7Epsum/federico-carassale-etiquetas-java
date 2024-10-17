23a08a851a8672777a8b845edecdc665937eb3be
F7Epsum <federico.carassale@estudiante.epsum.school>
2024-10-17 11:21:34 +0200

    Main solucionado

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+	public static void main (String[]args){
+		System.out.println(args.length);
+		if(args.length == 0) {
+		System.out.println("No Hay Argumentos");
+		} else {
+		System.out.println("Hay " + args.length + "argumentos");
+		}
+
+		try{
+			for(int i=0; i < args.length; i++){
+			System.out.println(args[i]);
+			}
+		} catch (IndexOutOfBoundsException e){
+			System.out.println("Error: " + e);
+		}
+	}
+}
