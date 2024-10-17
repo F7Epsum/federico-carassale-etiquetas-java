3260a67e70aabf3a3384b09e2280e13e628635e7
F7Epsum <federico.carassale@estudiante.epsum.school>
2024-10-17 10:43:59 +0200

    argumentos impresos en main

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 public class Main{
 	public static void main (String[]args){
-		System.out.println("");
+		System.out.println(args.length);
+		if(args.length == 0) {
+		System.out.println("No Hay Argumentos");
+		} else {
+		System.out.println("Hay " + args.length() + "argumentos");
+		}
 	}
 }
